94b294baa8c1230cde7ce7370845e080a975cb3c
VioletViola <vivian.v18723@gmail.com>
2021-08-15 15:29:46 +0100

    Comment out code in java.

---
+++ src/Comments.java
@@ -0,0 +1,14 @@
+public class Comments {
+    public static void main(String[] args) {
+        // This is a comment
+
+        /*
+            This is another comment,
+            a multiline comment!
+        */
+
+//        System.out.println("Hello World!"); Code got commented out using shortcuts
+
+        System.out.println("Hello World!"); // This is also a comment
+    }
+}
